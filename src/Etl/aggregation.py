ecc395d9cd8fe9002285607e5ad49dfdfd05139f
JulianBSL <julianbsanchezlopez@gmail.com>
2024-05-01 11:20:36 +0200

    Etl finished

--- src/Etl/aggregation.py
+++ src/Etl/aggregation.py
@@ -12,8 +12,14 @@ def fetch_data():
     connection = client.connect(os.getenv("HOST_CRATE"), username="crate", password=None, timeout=10)
     cursor = connection.cursor()
 
+    query = ("SELECT "
+             "entity_id, dateobserved, temperature, ph, turbidity, conductivity, level, chlorine "
+             "FROM etwaterquality "
+             "LIMIT 10")
+
     try:
-        cursor.execute("SELECT * FROM etwaterquality LIMIT 10")
+        cursor.execute(query)
+        # Fetch all with column names
         rows = cursor.fetchall()
         return rows
     except Exception as e:
@@ -24,5 +30,34 @@ def fetch_data():
 
 
 data_fetched = fetch_data()
-df_buoy = pd.DataFrame(data_fetched)
-print(df_buoy.head())
+df_buoy = pd.DataFrame(
+    data_fetched,
+    columns=["entity", "date_observed", "temperature", "ph", "turbidity", "conductivity", "level",
+             "chlorine"]
+)
+
+# Unpivot the DataFrame
+df_buoy = pd.melt(
+    df_buoy,
+    id_vars=["entity", "date_observed"],
+    value_vars=["temperature", "ph", "turbidity", "conductivity", "level", "chlorine"],
+    var_name="property",
+    value_name="value"
+)
+
+# Convert the date_observed to only keep the date part
+df_buoy["date_observed"] = pd.to_datetime(df_buoy["date_observed"], unit="ms")
+df_buoy["date_observed"] = df_buoy["date_observed"].dt.strftime("%Y-%m-%d")
+
+# Group by entity, date_observed, and property, and calculate the average, min, and max values
+df_buoy = df_buoy.groupby(["entity", "date_observed", "property"]).agg(
+    avg_value=("value", "mean"),
+    min_value=("value", "min"),
+    max_value=("value", "max")
+).reset_index()
+
+# Print the DataFrame with all columns
+print(df_buoy)
+
+# Save the DataFrame to a CSV file
+df_buoy.to_csv("./../../data/etwaterquality.csv", index=False)

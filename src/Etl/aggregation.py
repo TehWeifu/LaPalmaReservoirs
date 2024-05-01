a9e3d628a9c3fbbcf67c138d83fad1ee80e504ae
JulianBSL <julianbsanchezlopez@gmail.com>
2024-05-01 10:35:37 +0200

    Created grafana dashboard

--- src/Etl/aggregation.py
+++ src/Etl/aggregation.py
@@ -1,21 +1,27 @@
 import os
 
+import pandas as pd
 from crate import client
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Connect to CrateDB
-connection = client.connect(os.getenv("HOST_CRATE"), username="crate", password=None, timeout=10)
-cursor = connection.cursor()
 
-try:
-    cursor.execute("SELECT * FROM etwaterquality LIMIT 10")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-except Exception as e:
-    print("An error occurred:", e)
-finally:
-    cursor.close()
-    connection.close()
+def fetch_data():
+    # Connect to CrateDB
+    connection = client.connect(os.getenv("HOST_CRATE"), username="crate", password=None, timeout=10)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM etwaterquality LIMIT 10")
+        rows = cursor.fetchall()
+        return rows
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        cursor.close()
+        connection.close()
+
+
+data_fetched = fetch_data()
+df_buoy = pd.DataFrame(data_fetched)
